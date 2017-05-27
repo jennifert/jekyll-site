424ec583294202788c8576d2184822f613972ed0
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-26 22:57:11 -0400

    Updated deploy file
    To include moving to proper location. added atom file to demos for upcoming fix to javascript demos.

--- deploy.php
+++ deploy.php
@@ -1,9 +1,11 @@
 
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
+// Taken from: https://gist.github.com/oodavid/1809044
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' );
   shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
+  shell_exec( 'cd /var/www/_repo/;cp -f deploy.php /var/www/jenntesolin.com/public_html');
 }
 ?>
 Hello World.

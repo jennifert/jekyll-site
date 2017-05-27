19272fd102c5563131f91bc88559bdd52770a988
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-26 23:01:31 -0400

    another deploy fix.

--- deploy.php
+++ deploy.php
@@ -4,8 +4,8 @@
 // Taken from: https://gist.github.com/oodavid/1809044
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
   shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
-  shell_exec( 'cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
 }
 ?>
-Hello World.
+Hello.

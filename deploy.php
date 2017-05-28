8f2e2950cfee47c95f71f381fbc236052c3bd46c
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-28 15:45:33 -0400

    try to fix deploy.

--- deploy.php
+++ deploy.php
@@ -4,8 +4,8 @@
 
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' ); // Taken from: https://gist.github.com/oodavid/1809044
-  shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
-  shell_exec( 'cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
+  exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
+  exec( 'cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
 }
 ?>
 Hello :)

98b9449a3edc981996e61c6dd4ca90f260733c2d
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-28 15:41:22 -0400

    test

--- deploy.php
+++ deploy.php
@@ -4,7 +4,7 @@
 
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' ); // Taken from: https://gist.github.com/oodavid/1809044
-  shell_exec( 'cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
+  shell_exec( 'cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
   shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
 }
 ?>

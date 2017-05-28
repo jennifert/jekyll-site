263ed53a27f6461edc99357972fb2feb3a54469b
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-28 15:34:29 -0400

    attempt to fix

--- deploy.php
+++ deploy.php
@@ -1,9 +1,11 @@
 
 <?php
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull');
+// Gitweb hook.
 
-  shell_exec( 'cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/;cd _site;cp -fR * /var/www/jenntesolin.com/public_html/;cd /var/www/jenntesolin.com/public_html/;cp search.json search/search.json'); //change to move when new search up.
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' ); // Taken from: https://gist.github.com/oodavid/1809044
+  shell_exec( 'cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
+  shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
 }
 ?>
 Hello. :)

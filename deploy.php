19e3e2c7947372825bca05be01d7beae288ca98b
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-28 14:26:30 -0400

    Updates for upcoming search

--- deploy.php
+++ deploy.php
@@ -1,11 +1,10 @@
 
 <?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-// Taken from: https://gist.github.com/oodavid/1809044
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' );
   shell_exec( 'cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
   shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html');
+  shell_exec( 'cd /var/www/jenntesolin.com/public_html/;cp search.json search/search.json'); //change to move when new search up.
 }
 ?>
 Hello.

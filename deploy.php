e14d099dc54b09608b0effe3f323cf9a79c4c8f4
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-28 15:10:03 -0400

    edits

--- deploy.php
+++ deploy.php
@@ -1,10 +1,11 @@
 
 <?php
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull');
+
   shell_exec( 'cp -fR deploy.php /var/www/jenntesolin.com/public_html/');
   shell_exec( 'cd _site;cp -fR * /var/www/jenntesolin.com/public_html/');
   shell_exec( 'cd /var/www/jenntesolin.com/public_html/;cp search.json search/search.json'); //change to move when new search up.
 }
 ?>
-Hello.
+Hello world.

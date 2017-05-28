44b5b4fb3767c70efe59eb0f8d36f80018b40a90
Jennifer Tesolin <jtesolin@gmail.com>
2017-05-28 15:25:09 -0400

    removed sudo...

--- deploy.php
+++ deploy.php
@@ -3,7 +3,7 @@
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull');
 
-  shell_exec( 'sudo -S cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/;cd _site;cp -fR * /var/www/jenntesolin.com/public_html/;cd /var/www/jenntesolin.com/public_html/;cp search.json search/search.json'); //change to move when new search up.
+  shell_exec( 'cd /var/www/_repo/;cp -fR deploy.php /var/www/jenntesolin.com/public_html/;cd _site;cp -fR * /var/www/jenntesolin.com/public_html/;cd /var/www/jenntesolin.com/public_html/;cp search.json search/search.json'); //change to move when new search up.
 }
 ?>
 Hello world.

c3e844636cd6c41600384c3f0bca57d69581163d
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2020-02-06 20:28:04 -0500

    Delete deploy.php

--- deploy.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/_repo/ && git reset --hard HEAD && git pull' ); // Taken from: https://gist.github.com/oodavid/1809044
-}
-?>
-Hello :)
